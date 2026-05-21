a4160d931af8c3073e1cee03f19c2b230be96852
LKang <leeliankang1@gmail.com>
2026-05-21 10:42:11 +0800

    Profile update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -123,7 +123,8 @@ export default function Hero() {
 
             <div className="flex flex-wrap gap-4">
               <a
-                href="/Profile.pdf"
+                href="/Profile.jpg
+                "
                 className="rounded-full bg-red-800 px-7 py-4 text-base font-semibold text-white transition-colors hover:bg-red-700"
               >
                 View projects
